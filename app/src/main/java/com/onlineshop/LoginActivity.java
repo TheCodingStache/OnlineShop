56f9287948de05d86a00c6495e1b5ac088340fc9
dmspallas <dmspallas@gmail.com>
2019-05-15 05:18:29 +0300

    Implementing user login

--- app/src/main/java/com/onlineshop/LoginActivity.java
+++ app/src/main/java/com/onlineshop/LoginActivity.java
@@ -1,12 +1,77 @@
 package com.onlineshop;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button loginButton;
+    private ProgressDialog loadingBar;
+    private String parentDatabaseName = "Users";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loadingBar = new ProgressDialog(this);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserLogin();
+            }
+        });
+    }
+
+    private void UserLogin() {
+        String name = username.getText().toString();
+        String pass = password.getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(this, "Please write your username...", Toast.LENGTH_LONG).show();
+        } else if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_LONG).show();
+        } else {
+            loadingBar.setTitle("Please Login");
+            loadingBar.setMessage("Please wait while we are checking the credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            allowAccessToAccount(name, pass);
+        }
+    }
+
+    private void allowAccessToAccount(final String username, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference().child("posts");
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(parentDatabaseName).child(username).exists()) {
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Account with this " + username + " number do not exist", Toast.LENGTH_LONG).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/onlineshop/Prevalent/Prevalent.java
@@ -0,0 +1,4 @@
+package com.onlineshop.Prevalent;
+
+public class Prevalent {
+}
